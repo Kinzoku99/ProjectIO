c3867cae304c8b4ffd377dbb5eed5cc177b9dde3
Kinzoku99 <szymonek31@live.com>
2022-04-11 16:11:19 +0200

    oneline

--- frontend/src/mathOutputFunctions.tsx
+++ frontend/src/mathOutputFunctions.tsx
@@ -10,40 +10,9 @@ export const iEQ = (eq: string) => {
 }
 
 export const EQ: (eq: string) => (JSX.Element) = (eq: string) => {
-    if (eq.length >= LIMIT) {
-        let equations: Array<string> = [];
-        let components: Array<JSX.Element> = [];
-
-        let match: IterableIterator<RegExpMatchArray> = eq.matchAll(/[-+=]+/g);
-        let lastSplit = 0;
-        let next = match.next();
-
-        while (!next.done) {
-            const split = next.value;
-
-            if (split.index !== undefined && split.index - lastSplit >= LIMIT) {
-                equations.push(eq.substring(lastSplit, split.index) + ' ' + split[0]);
-                lastSplit = split.index;
-            }
-
-            next = match.next();
-        }
-
-        equations.push(eq.substring(lastSplit));
-
-        for (const element of equations) {
-            components.push(<MathComponent tex={element} display={true}/>)
-        }
-
         return (
-            <div className="math-multiline">
-                {components}
-            </div>
-        );
-    }
-    else {
-        return (
+			<div className="math-multiline">
             <MathComponent tex={eq} display={true}/>
+			</div>
         );
-    }
 }
